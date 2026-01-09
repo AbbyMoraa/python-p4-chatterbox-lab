a9c091ca31ba11930d0716176660a02285ef7a50
Abigael Momanyi <abbymoraa876@gmail.com>
2026-01-09 08:04:08 -0500

    app.py fixed

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
-from models import db, Message
+from models import db, Message  # your models.py should define Message with to_dict()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -10,14 +10,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
-migrate = Migrate(app, db)
 db.init_app(app)
+migrate = Migrate(app, db)
+
+# -----------------------------
+# ROUTES
+# -----------------------------
 
 # GET all messages
 @app.route('/messages', methods=['GET'])
-def messages():
-    messages = Message.query.order_by(Message.created_at.asc()).all()
-    return jsonify([m.to_dict() for m in messages])
+def get_messages():
+    all_messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([m.to_dict() for m in all_messages])
 
 # POST new message
 @app.route('/messages', methods=['POST'])
@@ -28,9 +32,9 @@ def create_message():
     db.session.commit()
     return jsonify(new_message.to_dict()), 201
 
-# GET, PATCH, DELETE by id
-@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
-def messages_by_id(id):
+# GET or PATCH by id
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH'])
+def message_by_id(id):
     message = Message.query.get_or_404(id)
 
     if request.method == 'GET':
@@ -42,10 +46,8 @@ def messages_by_id(id):
         db.session.commit()
         return jsonify(message.to_dict())
 
-    elif request.method == 'DELETE':
-        db.session.delete(message)
-        db.session.commit()
-        return '', 204
-
+# -----------------------------
+# RUN SERVER
+# -----------------------------
 if __name__ == '__main__':
     app.run(port=5555)
